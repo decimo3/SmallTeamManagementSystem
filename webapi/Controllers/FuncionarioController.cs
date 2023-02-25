545dc69756d4ce7466dd3aa165776cd68771f8fa
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-02-24 22:27:46 -0300

    GET from ID and POST Funcionario work

--- webapi/Controllers/FuncionarioController.cs
+++ webapi/Controllers/FuncionarioController.cs
@@ -1,7 +1,7 @@
 namespace mestreruan.api.controllers;
 
 [Microsoft.AspNetCore.Mvc.ApiController]
-[Microsoft.AspNetCore.Mvc.Route("[controller]")]
+[Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
 
 public class Funcionario : Microsoft.AspNetCore.Mvc.ControllerBase
 {
@@ -9,5 +9,30 @@ public class Funcionario : Microsoft.AspNetCore.Mvc.ControllerBase
   public IEnumerable<mestreruan.api.Models.Funcionario> Get()
   {
     return mestreruan.api.Model.FuncionarioDAO.recuperarFuncionarios();
+  }
+  [Microsoft.AspNetCore.Mvc.HttpGet("{re}", Name = "GetFuncionario")]
+  public Microsoft.AspNetCore.Mvc.ActionResult<mestreruan.api.Models.Funcionario> Get(int re)
+  {
+    try
+    {
+      return mestreruan.api.Model.FuncionarioDAO.recuperarFuncionario(re);
+    }
+    catch (System.InvalidOperationException)
+    {
+      return NotFound();
+    }
+  }
+  [Microsoft.AspNetCore.Mvc.HttpPost(Name = "PostFuncionario")]
+  public Microsoft.AspNetCore.Mvc.ActionResult Post(mestreruan.api.Models.Funcionario funcionario)
+  {
+    try
+    {
+      mestreruan.api.Model.FuncionarioDAO.inserirFuncionario(funcionario);
+      return NoContent();
+    }
+    catch (System.InvalidOperationException)
+    {
+      return BadRequest();
+    }
   }
 }
--- webapi/Model/FuncionarioDAO.cs
+++ webapi/Model/FuncionarioDAO.cs
@@ -15,8 +15,23 @@ public static class FuncionarioDAO
       connection.Close();
     }
   }
-  public static void inserirFuncionario(mestreruan.api.Models.Funcionario funcionario)
+  public static mestreruan.api.Models.Funcionario recuperarFuncionario(int re)
   {
-    
+    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    {
+      connection.Open();
+      return connection.QuerySingle<mestreruan.api.Models.Funcionario>(@$"SELECT * FROM funcionario WHERE re = '{@re}'");
+      connection.Close();
+    }
+  }
+  public static void inserirFuncionario(mestreruan.api.Models.Funcionario f)
+  {
+    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    {
+      connection.Open();
+      connection.Execute(@"INSERT INTO public.funcionario(cpf, re, matricula, nome, sobrenome, apelido, senha, funcao, situacao, escala) VALUES (@cpf, @re, @matricula, @nome, @sobrenome, @apelido, @senha, @funcao, @situacao, @escala)",
+        new {cpf = f.cpf, re = f.re, matricula = f.matricula, nome = f.nome, sobrenome = f.sobrenome, apelido = f.apelido, senha = f.senha, funcao = f.funcao, situacao = f.situacao, escala = f.escala});
+      connection.Close();
+    }
   }
 }
