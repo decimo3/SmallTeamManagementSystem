aba7d0dee1d9b3a8d7b8a10d50734c02c984d4ca
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-03-27 22:24:11 -0300

    removed redundant method

--- webapi/Controllers/FuncionariosController.cs
+++ webapi/Controllers/FuncionariosController.cs
@@ -1,17 +1,10 @@
 namespace mestreruan.api.controllers;
-
 [Microsoft.AspNetCore.Mvc.ApiController]
 [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
-
 public class Funcionarios : Microsoft.AspNetCore.Mvc.ControllerBase
 {
-  [Microsoft.AspNetCore.Mvc.HttpGet("{situacao}/{funcao}/{escala}",Name = "recuperarEletricistasAtivos")]
-  public IEnumerable<mestreruan.api.Models.Funcionario> Get(int situacao, int funcao, int escala)
-  {
-    return mestreruan.api.Model.FuncionarioDAO.recuperarFuncionarios(situacao: situacao, funcao: funcao, escala: escala);
-  }
-  [Microsoft.AspNetCore.Mvc.HttpGet("{situacao}/{funcao}", Name = "recuperarEletricistas")]
-  public IEnumerable<mestreruan.api.Models.Funcionario> Get(int situacao, int funcao)
+  [Microsoft.AspNetCore.Mvc.HttpGet("{situacao}/{funcao}",Name = "recuperarEletricistasAtivos")]
+  public IEnumerable<mestreruan.api.Models.Funcionario> Get(mestreruan.api.Models.Funcionario.Situacao situacao, mestreruan.api.Models.Funcionario.Funcao funcao)
   {
     return mestreruan.api.Model.FuncionarioDAO.recuperarFuncionarios(situacao: situacao, funcao: funcao);
   }
