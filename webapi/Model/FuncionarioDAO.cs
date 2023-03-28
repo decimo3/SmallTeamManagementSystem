a267aa57a0c6253188cd4b4a88882d4f7478aa4e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-03-27 22:23:19 -0300

    swapped dapper methods for ef-core methods

--- webapi/Model/FuncionarioDAO.cs
+++ webapi/Model/FuncionarioDAO.cs
@@ -1,82 +1,75 @@
-// using System.Data;
-// using Microsoft.Data.SqlClient;
-using Dapper;
-using Dapper.Contrib.Extensions;
-
+using mestreruan.api.Models;
+using mestreruan.api.Database;
 namespace mestreruan.api.Model;
 public static class FuncionarioDAO
 {
-  private static string connectionString = $"Host=localhost;Username=usuario;Password=123456789;Database=aplicacao";
-  public static IEnumerable<mestreruan.api.Models.Funcionario> recuperarFuncionarios(int situacao = 0, int funcao = 0, int escala = 0)
+  public static List<Funcionario> recuperarFuncionarios(Funcionario.Situacao situacao, Funcionario.Funcao funcao)
   {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    Funcionario.Escala? escala;
+    switch(DateTime.Today.DayOfWeek)
     {
-      connection.Open();
-      return connection.Query<mestreruan.api.Models.Funcionario>(@$"SELECT * FROM funcionario WHERE funcao = {funcao} AND situacao = {situacao} AND escala = {escala}");
-      connection.Close();
+      case DayOfWeek.Monday:
+        escala = Funcionario.Escala.segSex;
+        break;
+      case DayOfWeek.Tuesday:
+      case DayOfWeek.Wednesday:
+      case DayOfWeek.Thursday:
+      case DayOfWeek.Friday:
+        escala = null;
+        break;
+      case DayOfWeek.Saturday:
+        escala = Funcionario.Escala.terSab;
+        break;
+      default:
+        throw new InvalidOperationException("Não há ninguém disponível no domingo");
     }
-  }
-  public static IEnumerable<mestreruan.api.Models.Funcionario> recuperarFuncionarios(int situacao = 0, int funcao = 0)
-  {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    using(var dbContext = new DataBaseContext())
     {
-      connection.Open();
-      return connection.Query<mestreruan.api.Models.Funcionario>(@$"SELECT * FROM funcionario WHERE funcao = {funcao} AND situacao = {situacao}");
-      connection.Close();
-    }
-  }
-  public static IEnumerable<mestreruan.api.Models.Funcionario> recuperarFuncionarios()
-  {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
-    {
-      connection.Open();
-      return connection.Query<mestreruan.api.Models.Funcionario>(@$"SELECT * FROM funcionario");
-      connection.Close();
+      if(escala == null)
+      {
+        return (from f in dbContext.Funcionarios
+                where (f.funcao == funcao && f.situacao == situacao && f.escala == escala)
+                select new Funcionario()).ToList();
+      }
+      else
+      {
+        return (from f in dbContext.Funcionarios
+                where (f.funcao == funcao && f.situacao == situacao)
+                select new Funcionario()).ToList();
+      }
     }
   }
   public static mestreruan.api.Models.Funcionario recuperarFuncionario(int re)
   {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    using(var dbContext = new DataBaseContext())
     {
-      connection.Open();
-      return connection.QuerySingle<mestreruan.api.Models.Funcionario>(@$"SELECT * FROM funcionario WHERE re = '{@re}'");
-      connection.Close();
+      return (from f in dbContext.Funcionarios
+              where f.re == re
+              select new Funcionario()).Single();
     }
   }
   public static void inserirFuncionario(mestreruan.api.Models.Funcionario funcionario)
   {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    using(var dbContext = new DataBaseContext())
     {
-      connection.Open();
-      connection.Insert<mestreruan.api.Models.Funcionario>(funcionario);
-      connection.Close();
+      dbContext.Funcionarios.Add(funcionario);
+      dbContext.SaveChanges();
     }
   }
   public static void alterarFuncionario(mestreruan.api.Models.Funcionario funcionario)
   {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    using(var dbContext = new DataBaseContext())
     {
-      connection.Open();
-      connection.Update<mestreruan.api.Models.Funcionario>(funcionario);
-      connection.Close();
+      dbContext.Funcionarios.Update(funcionario);
+      dbContext.SaveChanges();
     }
   }
   public static void apagarFuncionario(mestreruan.api.Models.Funcionario funcionario)
   {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
+    using(var dbContext = new DataBaseContext())
     {
-      connection.Open();
-      connection.Delete<mestreruan.api.Models.Funcionario>(funcionario);
-      connection.Close();
-    }
-  }
-  public static IEnumerable<mestreruan.api.Models.Funcionario> recuperarFuncionarios(string funcao)
-  {
-    using(var connection = new Npgsql.NpgsqlConnection(connectionString))
-    {
-      connection.Open();
-      return connection.Query<mestreruan.api.Models.Funcionario>(@$"SELECT * FROM funcionario WHERE funcao = 0");
-      connection.Close();
+      dbContext.Funcionarios.Remove(funcionario);
+      dbContext.SaveChanges();
     }
   }
 }
