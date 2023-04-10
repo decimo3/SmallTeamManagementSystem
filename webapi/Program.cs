828de4f8deb20809ca31530430962a3310a86a55
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-04-10 18:47:48 -0300

    Setting environments variables if is in development

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,30 +1,21 @@
-var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(new WebApplicationOptions
-    {
-        Args = args,
-        EnvironmentName = Environments.Development
-    });
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder();
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 var app = builder.Build();
-
-app.Urls.Add($"http://*:{System.Environment.GetEnvironmentVariable("WEBAPI_PORT")}");
-// app.Urls.Add("https://*:4433");
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    dotenv.net.DotEnv.Fluent().WithEnvFiles("../.env").Load();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+var port = System.Environment.GetEnvironmentVariable("WEBAPI_PORT");
+app.Urls.Add($"http://*:{port}");
+// app.Urls.Add("https://*:4433");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
