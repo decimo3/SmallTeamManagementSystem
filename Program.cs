6000b9453362c561eda5686696fb101b402fe0cc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-06-26 19:49:47 -0300

    Added existing dependencies, services and helpers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using sge.Services;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<DataBaseContext>();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -11,10 +15,17 @@ if (!app.Environment.IsDevelopment())
     // see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+if (app.Environment.IsDevelopment())
+{
+    dotenv.net.DotEnv.Fluent().WithEnvFiles(".env").Load();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+app.UseMiddleware<AuthMiddleware>();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
