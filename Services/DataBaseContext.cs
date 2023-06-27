72c7a79623dffa95fb3e7b2f9e7af43a21196e53
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-06-26 21:55:21 -0300

    Using different databases for development and production

--- Services/DataBaseContext.cs
+++ Services/DataBaseContext.cs
@@ -3,13 +3,26 @@ using sge.Entities;
 namespace sge.Services;
 public class DataBaseContext : DbContext
 {
-  private string server = System.Environment.GetEnvironmentVariable("POSTGRES_HOST");
-  private string dbuser = System.Environment.GetEnvironmentVariable("POSTGRES_USER");
-  private string dbpass = System.Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-  private string dbbase = System.Environment.GetEnvironmentVariable("POSTGRES_DB");
+  private readonly IWebHostEnvironment _env;
+
+  public DataBaseContext(IWebHostEnvironment env)
+  {
+    _env = env;
+  }
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    optionsBuilder.UseNpgsql($"Host={server};Username={dbuser};Password={dbpass};Database={dbbase}");
+    if(_env.IsDevelopment())
+    {
+      optionsBuilder.UseSqlite("Data Source=database.db");
+    }
+    if(_env.IsProduction())
+    {
+      string server = System.Environment.GetEnvironmentVariable("POSTGRES_HOST");
+      string dbuser = System.Environment.GetEnvironmentVariable("POSTGRES_USER");
+      string dbpass = System.Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+      string dbbase = System.Environment.GetEnvironmentVariable("POSTGRES_DB");
+      optionsBuilder.UseNpgsql($"Host={server};Username={dbuser};Password={dbpass};Database={dbbase}");
+    }
   }
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
