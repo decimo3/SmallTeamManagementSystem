85e4491fb4ecfc085aaa03661db2b731dfeff5ba
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-06-26 22:13:22 -0300

    Just to reduce possibly null reference warnings

--- Services/AuthAtributo.cs
+++ Services/AuthAtributo.cs
@@ -5,7 +5,7 @@ public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 {
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        var user = (sge.Entities.Funcionario)context.HttpContext.Items["User"];
+        var user = (sge.Entities.Funcionario?)context.HttpContext.Items["User"];
         // Se a variável 'user' for nulo, quer dizer que o usuário não está logado!
         if (user == null) context.Result = new Microsoft.AspNetCore.Mvc.JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
     }
--- Services/DataBaseContext.cs
+++ Services/DataBaseContext.cs
@@ -17,10 +17,10 @@ public class DataBaseContext : DbContext
     }
     if(_env.IsProduction())
     {
-      string server = System.Environment.GetEnvironmentVariable("POSTGRES_HOST");
-      string dbuser = System.Environment.GetEnvironmentVariable("POSTGRES_USER");
-      string dbpass = System.Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-      string dbbase = System.Environment.GetEnvironmentVariable("POSTGRES_DB");
+      string? server = System.Environment.GetEnvironmentVariable("POSTGRES_HOST");
+      string? dbuser = System.Environment.GetEnvironmentVariable("POSTGRES_USER");
+      string? dbpass = System.Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+      string? dbbase = System.Environment.GetEnvironmentVariable("POSTGRES_DB");
       optionsBuilder.UseNpgsql($"Host={server};Username={dbuser};Password={dbpass};Database={dbbase}");
     }
   }
