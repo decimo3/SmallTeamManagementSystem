21dee2e4f4dc49e4a25d2900866c7ee1c63752a2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-07-25 19:30:54 -0300

    Defining the display name for the model

--- Models/AuthRequest.cs
+++ Models/AuthRequest.cs
@@ -3,7 +3,9 @@ using System.ComponentModel.DataAnnotations;
 public class AuthRequest
 {
     [Required]
+    [Display(Name="Matrícula")]
     public int matricula {get; set;}
     [Required]
+    [Display(Name="Senha")]
     public string palavra {get; set;}
 }
