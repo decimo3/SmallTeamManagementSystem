62a3c6ce9c97de5b23ebebe8cbae65fa255b3b73
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-07-31 17:43:00 -0300

    Done: AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,14 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using sge.Models;
+using sge.Services;
 namespace sge.Controllers;
 public class AuthController : Controller
 {
+  private readonly DataBaseContext _context;
+  private readonly IAuthService _service;
+  public AuthController(DataBaseContext context, IAuthService service)
+  {
+    _context = context;
+    _service = service;
+  }
+  [HttpGet]
   public IActionResult Login()
   {
     return View();
   }
+  [HttpGet]
   public IActionResult Recovery()
   {
     return View();
+  }
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult Login(AuthRequest authRequest)
+  {
+    if(!ModelState.IsValid) return View("Login");
+    var token = _service.Authenticate(authRequest);
+    if(token is null) return View("Login");
+    return View("Login");// Ok(token)
   }
 }
